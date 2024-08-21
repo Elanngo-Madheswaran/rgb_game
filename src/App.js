c5526f1931225cf9512acc4179ee55988c7b577e
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-08-21 22:45:43 +0530

    added no of options selector

--- src/App.js
+++ src/App.js
@@ -20,7 +20,8 @@ class App extends Component {
     super(props);
     this.state = {
       selectedcol: '', // Currently displayed color
-      colorOptions: [] // Array to hold multiple RGB color options
+      colorOptions: [], // Array to hold multiple RGB color options
+      numberOfOptions: 4 // Default number of options
     };
   }
 
@@ -57,10 +58,19 @@ class App extends Component {
 
   updateColors = () => {
     const selectedColor = this.generateRandomColor();
-    const otherColors = this.generateMultipleColors(3, selectedColor); // Generate other colors excluding the selected color
+    const otherColors = this.generateMultipleColors(this.state.numberOfOptions - 1, selectedColor); // Generate other colors excluding the selected color
     const allColors = [...otherColors, selectedColor]; // Add the selected color to the list
     this.setState({ 
       selectedcol: selectedColor, 
+      colorOptions: this.shuffleArray(allColors) // Shuffle to randomize the order
+    });
+  };
+
+  updateColorOptions = () => {
+    const { selectedcol, numberOfOptions } = this.state;
+    const otherColors = this.generateMultipleColors(numberOfOptions - 1, selectedcol); // Generate other colors excluding the selected color
+    const allColors = [...otherColors, selectedcol]; // Add the selected color to the list
+    this.setState({ 
       colorOptions: this.shuffleArray(allColors) // Shuffle to randomize the order
     });
   };
@@ -74,17 +84,30 @@ class App extends Component {
     }
   };
 
+  handleOptionChange = (event) => {
+    this.setState({ numberOfOptions: parseInt(event.target.value) }, this.updateColorOptions);
+  };
+
   render() {
-    const { selectedcol, colorOptions } = this.state;
+    const { selectedcol, colorOptions, numberOfOptions } = this.state;
     return (
       <div className="flex flex-col items-center m-5 p-5">
         <h1 className="text-6xl text-center font-bold m-5 p-5">RGB GAME</h1>
-          <div className="w-48 sm:w-96 sm:h-96 h-48" 
-            style={{ 
-              backgroundColor: selectedcol
-            }} 
-          />
-          <p className="sm:text-3xl text-xl font-medium self-start sm:m-5 sm:p-5 m-2 p-2 mt-5 pt-5 ">Choose the right colour value:</p>
+        <div className="w-48 sm:w-96 sm:h-96 h-48" 
+          style={{ 
+            backgroundColor: selectedcol
+          }} 
+        />
+        <div className="flex">
+          <p className="sm:text-3xl text-xl font-medium self-start sm:m-5 sm:p-5 m-2 p-2 mt-5 pt-5">No of options you want:</p>
+          <select id="noofopt" className="bg-slate-200 border-2 border-black w-24 h-12 self-center" value={numberOfOptions} onChange={this.handleOptionChange}>
+            <option value={2}>2</option>
+            <option value={4}>4</option>
+            <option value={8}>8</option>
+          </select>
+
+        </div>
+        <p className="sm:text-3xl text-xl font-medium self-start sm:m-5 sm:p-5 m-2 p-2 mt-5 pt-5">Choose the right colour value:</p>
         <div className="flex justify-center flex-wrap gap-10">
           {colorOptions.map((color, index) => (
             <Cir
